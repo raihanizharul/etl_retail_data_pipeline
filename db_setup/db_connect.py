7297772e61d1eda058cb3b3fae159e750cda6a9d
raihan <raihanizharul7@gmail.com>
2026-09-04 20:18:58 +0700

    add script to connect to database

---
+++ db_setup/db_connect.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv(override=True)
+
+def get_engine():
+    return create_engine(
+        f"postgresql+psycopg2://"
+        f"{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+    )
+
+
